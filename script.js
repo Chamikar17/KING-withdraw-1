9448ba1df3dea01fc48e4331d0631b2cf57e14ee
Chamikar17 <gitseasen@gmail.com>
2025-05-28 06:28:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const links = {
   dialog: "https://wa.me/+94767861594?text=Dialog+card+for+free+my+usarmeme=",
-  hutch: "https://your-hutch-card-link.com",
-  mobitel: "https://your-mobitel-card-link.com",
-  dialogtv: "https://your-dialog-tv-link.com",
-  ff: "https://your-ff-topup-link.com"
+  hutch: "https://wa.me/+94767861594?text=Hutch+card+for+free+my+usarmeme=",
+  mobitel: "https://wa.me/+94767861594?text=Mobitel+card+for+free+my+usarmeme=",
+  dialogtv: "https://wa.me/+94767861594?text=Dialog+TV+card+for+free+my+usarmeme=",
+  ff: "https://wa.me/+94767861594?text=FF+JEM+for+free+my+usarmeme="
 };
 
 function openLink(key) {
